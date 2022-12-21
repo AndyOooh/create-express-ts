0fb1298c73750121b11cff9535b65955fc8e0737
AndyOoh <andy.oeee@gmail.com>
2022-12-22 02:38:05 +0700

    delete .vscode, remove bin from .gitignore, add .git deleteion in cli.js

---
+++ bin/cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import { execSync } from 'child_process';
+
+const runCommand = command => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (e) {
+    console.log(`Failed to execute ${command}`, e);
+    return false;
+  }
+  return true;
+};
+
+const projectName = process.argv[2];
+const repoName = 'create-express-ts';
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/AndyOooh/${repoName} ${projectName}`;
+const deleteGitCommand = `sudo rm -rf .git`;
+const installDepsCommand = `cd ${projectName} && yarn install`;
+
+
+console.log(`Cloning the ${repoName} repository to folder: ${projectName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) {
+  process.exit(1);
+}
+
+console.log('Installing dependencies...');
+const installedDependencies = runCommand(installDepsCommand);
+if (!installedDependencies) {
+  process.exit(1);
+}
+console.log('Removing .git');
+runCommand(deleteGitCommand);
+
+console.log('Done!');
+console.log(`cd into ${projectName} and run "yarn start" to start the server.`);
